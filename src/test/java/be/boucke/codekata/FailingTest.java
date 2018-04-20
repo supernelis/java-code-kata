234baa31e741556ade3e530ebd8527f8a004f52f
Nelis Boucké <nelis@archiwise.com>
2018-04-20 10:20:10 +0200

    Empty project to start kata

---
+++ src/test/java/be/boucke/codekata/FailingTest.java
@@ -0,0 +1,14 @@
+package be.boucke.codekata;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+
+public class FailingTest {
+
+    @Test
+    public void failingTest(){
+        assertThat(true,equalTo(true));
+    }
+}
